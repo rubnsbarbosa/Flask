d4deb429a0eed2535036e4818d5aea5b90cdc9e5
rubnsbarbosa <rubensbarbosa90@gmail.com>
2020-07-06 13:47:45 -0300

    routes

--- app/controllers/default.py
+++ app/controllers/default.py
@@ -4,3 +4,16 @@ from app import app
 @app.route('/')
 def index():
     return "hello world"
+
+@app.route('/test', defaults={'name' : None})
+@app.route('/test/<name>')
+def test(name):
+    if name:
+        return 'hello %s' % name
+    else:
+        return "hello"
+
+@app.route('/testid/<int:id>', methods=['GET'])
+def test_id(id):
+    print(type(id))
+    return 'hi'
